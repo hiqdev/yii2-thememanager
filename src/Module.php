0d9f909ed6d15c2436a2d0c04fd1f91c37c3293f
Andrii Vasyliev <sol@hiqdev.com>
2016-08-18 14:11:56 +0000

    + isHomePage()

--- src/Module.php
+++ src/Module.php
@@ -11,7 +11,19 @@
 
 namespace hiqdev\thememanager;
 
+use Yii;
+
 class Module extends \yii\base\Module
 {
     use GetManagerTrait;
+
+    /**
+     * @return bool
+     */
+    public static function isHomePage()
+    {
+        $controller = Yii::$app->controller;
+        $default_controller = Yii::$app->defaultRoute;
+        return (($controller->id === $default_controller) && ($controller->action->id === $controller->defaultAction));
+    }
 }
