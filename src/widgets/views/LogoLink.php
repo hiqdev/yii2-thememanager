1f5437902e6bd5dab8e09996bc48ddf32b2137f1
tofid <andreyklochok@gmail.com>
2016-10-20 18:13:08 +0300

    Added php doc, added options to Html::img

--- src/widgets/views/LogoLink.php
+++ src/widgets/views/LogoLink.php
@@ -1,10 +1,14 @@
 <?php
 
+/** @var array $options */
+/** @var string $image */
+/** @var string $url */
+
 use yii\helpers\Html;
 
 ?>
 <?php if (!empty($image)) : ?>
-    <?= Html::a(Html::img($image), $url) ?>
+    <?= Html::a(Html::img($image, $options), $url) ?>
 <?php else : ?>
-    <?= Html::a($name, $url) ?>
+    <?= Html::a($name, $url, $options) ?>
 <?php endif ?>
