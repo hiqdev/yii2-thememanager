f92ab1d1430bc49dd42ea06f1ac00b1e6aed22fa
Andrii Vasyliev <sol@hiqdev.com>
2016-12-21 10:31:49 +0000

    + Flashes widget (empty)

---
+++ src/widgets/Flashes.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Theme Manager for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-thememanager
+ * @package   yii2-thememanager
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\thememanager\widgets;
+
+/**
+ * Flashes widget. Displays flashes.
+ *
+ * @see `hiqdev/yii2-pnotify` for example of implementation.
+ */
+class Flashes extends \yii\base\Widget
+{
+}
