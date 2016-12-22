c829c7432b24934e0c627859ec6079a58920ad65
Andrii Vasyliev <sol@hiqdev.com>
2016-12-22 13:57:26 +0000

    + AbstractFooterMenu

---
+++ src/menus/AbstractFooterMenu.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\thememanager\menus;
+
+/**
+ * Abstract Footer Menu.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+abstract class AbstractFooterMenu extends AbstractMenu
+{
+}
