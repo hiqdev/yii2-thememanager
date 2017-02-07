ee3630ea9d73b93c79dd49bbc25c6665109fd150
tofid <andreyklochok@gmail.com>
2017-02-07 20:03:43 +0200

    Added Faq assets

--- src/FaqAsset.php
+++ src/FaqAsset.php
@@ -1,19 +1,25 @@
 <?php
 
-namespace hiqdev\themes\dataserv;
+namespace hiqdev\thememanager;
 
+use yii\bootstrap\BootstrapAsset;
 use yii\web\AssetBundle;
 use yii\web\JqueryAsset;
 
 class FaqAsset extends AssetBundle
 {
-    public $sourcePath = '@hiqdev/themes/dataserv/assets/faq';
+    public $sourcePath = '@hiqdev/thememanager/assets/faq';
+
+    public $css = [
+        'css/faq.css',
+    ];
 
     public $js = [
         YII_ENV_PROD ? 'js/faq.min.js' : 'js/faq.js',
     ];
 
     public $depends = [
+        BootstrapAsset::class,
         JqueryAsset::class,
     ];
 }
