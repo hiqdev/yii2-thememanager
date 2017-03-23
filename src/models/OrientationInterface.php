76f5f2c7e4928056272fa6301dfa641e469eb2ba
tofid <andreyklochok@gmail.com>
2017-03-23 19:11:33 +0200

    Added OrientationInterface

---
+++ src/models/OrientationInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace hiqdev\thememanager\models;
+
+interface OrientationInterface
+{
+    const ORIENTATION_HORIZONTAL = 'horizontal';
+    const ORIENTATION_VERTICAL = 'vertical';
+}
