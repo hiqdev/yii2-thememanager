364fdff2f556e78cd0a9086f9ef2a1506cc273ad
tofid <andreyklochok@gmail.com>
2017-08-18 13:39:19 +0300

    Deleted FAQ js from root

--- src/widgets/views/faq/root.php
+++ src/widgets/views/faq/root.php
@@ -5,24 +5,6 @@ use yii\helpers\Html;
 /** @var array $items */
 /** @var \yii\web\View $this */
 /** @var \hipanel\site\widgets\Faq $this ->context */
-$this->registerJs("
-// Switch signs plus and minus on collapse
-$('.collapse').on('show.bs.collapse', function(event){
-    var i = $(this).siblings().find('i').eq(0);
-    i.toggleClass('fa-plus-square-o fa-minus-square-o');
-    event.stopPropagation();
-}).on('hide.bs.collapse', function(event){
-    var i = $(this).siblings().find('i').eq(0);
-    i.toggleClass('fa-minus-square-o fa-plus-square-o');
-    event.stopPropagation();
-});
-
-//  Select first tab
-$('.faq-tabs .faq-categories li:eq(0) a').tab('show');
-
-// Init FAQ plugin
-$('#faq').faq();
-");
 ?>
 11111111111111111111111111111111111111111111
 <div id="faq" class="faq-tabs">
