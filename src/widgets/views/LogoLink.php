4ffa8a26159a5b16ec1ea86eaa504b91ab4fc456
Andrii Klochok <andreyklochok@gmail.com>
2023-11-13 12:21:24 +0200

    AHN-1579

--- src/widgets/views/LogoLink.php
+++ src/widgets/views/LogoLink.php
@@ -1,16 +1,30 @@
 <?php
 
+use yii\helpers\Html;
+
 /** @var array $options */
 /** @var string $image */
 /** @var string $url */
-use yii\helpers\Html;
+/** @var string $name */
+
+// SEO title
+$this->registerCss(<<<CSS
+.header .logo > h2 {
+    position: absolute;
+    top: -10000px;
+    left: -10000px;
+}
+CSS
+);
 
 ?>
 
 <?php if (!empty($image)) : ?>
     <?= Html::beginTag('a', array_merge(['href' => $url, 'class' =>'logo'], $options)) ?>
-        <?= Html::img($image, $imageOptions) ?>
+        <?= Html::img($image, $imageOptions ?? []) ?>
     <?= Html::endTag('a') ?>
 <?php else : ?>
     <?= Html::a($name, $url, $options) ?>
 <?php endif ?>
+
+<?= Html::tag('h2', $name) ?>
